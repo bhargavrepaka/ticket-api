d239adba8ecc75faba14bfae99747e589c34a3e3
Repaka Bhargav <repaka.bhargav@gmail.com>
2023-08-12 16:16:05 +0530

    npm remove helmet

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import helmet from "helmet"
+
 import cors from "cors"
 import express from "express"
 import morgan from "morgan"
@@ -15,7 +15,6 @@ const port = process.env.PORT || 3000
 
 //Middleware
 app.use(cors())
-app.use(helmet())
 app.use(morgan("tiny"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
